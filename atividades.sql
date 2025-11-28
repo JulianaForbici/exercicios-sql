d638dc88213c79dcbbcd066b7fad55373df9739d
Juliana Forbici <105084941+JulianaForbici@users.noreply.github.com>
2025-11-28 08:21:27 -0300

    Add SQL queries for activities and data manipulation
    Added various SQL queries for selecting, updating, and deleting records, along with join operations and aggregate functions.

---
+++ atividades.sql
@@ -0,0 +1,227 @@
+select * from atividade order by id_atividade;
+select * from aluno order by id_aluno;
+select * from instrutor order by id_instrutor;
+select * from turma order by id_turma;
+select * from matricula order by id_aluno;
+select * from matricula order by id_turma;
+select * from frequencia order by id_aluno;
+ 
+------------------------------------------ //
+ 
+-- ATIVIDADES DE SELECT, UPDATE E DELETE
+ 
+select id_aluno, * from aluno where nome ='Rafael Souza';
+select id_aluno, * from aluno where nome ='Julio Augusto';
+select id_instrutor, * from instrutor where nome = 'Bernardo Souza';
+ 
+update aluno set peso = '89.0' where id_aluno = 4;
+update instrutor set dt_nascimento = '1999-10-12' where id_instrutor = 2;
+ 
+delete from frequencia where id_aluno = 8;
+delete from matricula where id_aluno = 8;
+delete from aluno where id_aluno = 8;
+ 
+------------------------------------------ //
+ 
+-- ATIVIDADES DE JOIN
+ 
+-- 1
+ 
+select a.nome as atividade, t.horario, t.duracao
+from turma t
+join atividade a on t.id_atividade = a.id_atividade
+where a.id_atividade in (10, 1);
+ 
+-- 2
+
+select a.nome as atividade, t.horario, t.duracao, i.nome as instrutor
+from turma t
+left join instrutor i on t.id_instrutor = i.id_instrutor
+join atividade a on t.id_atividade = a.id_atividade
+where a.nome in ('Ginástica', 'Circo')
+order by t.horario;
+ 
+-- 3
+
+select a.nome as atividade, t.horario, t.duracao, i.nome as instrutor
+from turma t
+left join instrutor i on t.id_instrutor = i.id_instrutor
+join atividade a on t.id_atividade = a.id_atividade
+where t.horario between time '12:00' and time '18:59'
+order by t.horario;
+
+ 
+-- 4
+ 
+select t.horario, i.nome as instrutor, t.duracao
+from turma t
+join instrutor i on t.id_instrutor = i.id_instrutor
+where i.nome = 'Bernardo Souza'
+order by t.horario;
+
+-- 5
+
+select a.nome, m.dt_matricula
+from matricula m
+join aluno a on m.id_aluno = a.id_aluno
+order by m.dt_matricula desc
+limit 5;
+ 
+-- 6
+
+select distinct a.nome
+from aluno a
+join matricula m on a.id_aluno = m.id_aluno
+order by a.nome
+ 
+-- 7
+
+select a.nome
+from aluno a
+left join matricula m on a.id_aluno = m.id_aluno
+where m.id_turma is null
+order by a.nome;
+ 
+-- 8
+
+select act.nome as atividade, t.horario, t.duracao
+from turma t
+join atividade act on t.id_atividade = act.id_atividade
+where t.id_instrutor is null
+order by t.horario;
+
+ 
+-- 9
+
+select i.nome as instrutor, act.nome as atividade, t.horario, t.duracao
+from turma t
+join instrutor i on t.id_instrutor = i.id_instrutor
+join atividade act on t.id_atividade = act.id_atividade
+order by i.nome, act.nome, t.horario;
+ 
+-- 10
+
+select i.nome as instrutor, a.nome as atividade, t.horario, t.duracao
+from turma t
+left join instrutor i on t.id_instrutor = i.id_instrutor
+join atividade a on t.id_atividade = a.id_atividade
+order by i.nome nulls last, a.nome, t.horario;
+ 
+-- 11
+
+select nome,
+extract(day from dt_nascimento) as dia,
+to_char(dt_nascimento, 'tmmonth') as mes
+from aluno
+where dt_nascimento is not null
+and extract(month from dt_nascimento) between 7 and 12
+order by extract(month from dt_nascimento), extract(day from dt_nascimento);
+
+------------------------------------------ //
+
+-- ATIVIDADES DML PARTE 2
+
+-- 1
+
+select count(*) as total alunos from aluno;
+
+-- 2 
+
+select count(*) as total instrutores from instrutor;
+
+-- 3
+
+select a.nome as atividade,
+count(distinct t.id_instrutor) as qtd_instrutores
+from atividade a
+left join turma t on t.id_atividade = a.id_atividade
+group by a.nome
+order by qtd_instrutores desc, a.nome;
+
+-- 4
+
+select a.nome as atividade,
+count(t.id_turma) as qtd_turmas,
+count(distinct t.id_instrutor) as qtd_instrutores
+from atividade a
+left join turma t on t.id_atividade = a.id_atividade
+group by a.nome
+order by a.nome;
+
+-- 5
+
+select distinct i.id_instrutor, i.nome
+from instrutor i
+join turma t on t.id_instrutor = i.id_instrutor
+order by i.nome;
+
+-- 6
+
+select i.id_instrutor, i.nome
+from instrutor i
+left join turma t on t.id_instrutor = i.id_instrutor
+where t.id_turma is null
+order by i.nome;
+
+-- 7
+
+select a.nome as atividade,
+count(t.id_turma) as qtd_turmas
+from atividade a
+left join turma t on t.id_atividade = a.id_atividade
+group by a.nome
+order by qtd_turmas desc, a.nome;
+
+-- 8
+
+select a.id_aluno, a.nome,
+count(m.id_turma) as qtd_turmas
+from aluno a
+join matricula m on m.id_aluno = a.id_aluno
+group by a.id_aluno, a.nome
+having count(m.id_turma) >= 2
+order by qtd_turmas desc, a.nome
+
+-- 9
+
+select act.nome as atividade,
+t.id_turma,
+t.horario,
+t.duracao,
+t.dt_inicio,
+count(m.id_aluno) as qtd_alunos
+from turma t
+join atividade act on t.id_atividade = act.id_atividade
+left join matricula m on m.id_turma = t.id_turma
+group by t.id_turma, act.nome
+order by act.nome, t.horario;
+
+-- 10 
+select act.nome as atividade,
+       count(m.id_aluno) as total_alunos
+from atividade act
+join turma t on t.id_atividade = act.id_atividade
+left join matricula m on m.id_turma = t.id_turma
+group by act.nome
+order by total_alunos desc
+limit 1;
+
+-- 11 
+select a.id_aluno, a.nome,
+count(f.id_frequencia) as presencas
+from aluno a
+join frequencia f on f.id_aluno = a.id_aluno
+where f.presente = true
+group by a.id_aluno, a.nome
+order by presencas desc
+limit 1;
+
+-- 12 
+select a.id_aluno, a.nome,
+count(f.id_frequencia) as presencas
+from aluno a
+join frequencia f on f.id_aluno = a.id_aluno
+where f.presente = true
+group by a.id_aluno, a.nome
+having count(f.id_frequencia) >= 3
+order by presencas desc, a.nome;
