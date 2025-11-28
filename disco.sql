c0f501ff8eb1f3cbcc001dffa3ff6b1dee788024
Juliana Forbici <105084941+JulianaForbici@users.noreply.github.com>
2025-11-28 17:02:55 -0300

    Create database schema for music-related tables

---
+++ disco.sql
@@ -0,0 +1,98 @@
+create table endereco(
+id_endereco int not null,
+rua varchar(255) not null,
+cidade varchar(50) not null,
+cep varchar(8), 
+estado varchar(30),
+telefone_residencial varchar(20),
+primary key (id_endereco));
+
+
+create table musico(
+numero_bi varchar(11) not null,
+nome varchar(255) not null,
+telefone varchar(20),
+id_endereco int references endereco(id_endereco),  
+primary key (numero_bi),
+foreign key (id_endereco) references endereco (id_endereco));
+
+create table instrumentos(
+id_instrumento int not null,
+nome varchar(100) not null,
+primary key (id_instrumento));
+
+create table musico_instrumento(
+numero_bi varchar(11) not null,
+id_instrumento int not null,
+primary key (id_instrumento),
+foreign key (numero_bi) references musico(numero_bi),
+foreign key (id_instrumento) references instrumentos (id_instrumento));
+
+create table gravadora(
+id_gravadora int not null,
+nome varchar(155),
+primary key (id_gravadora));
+
+create table artista(
+id_gravadora int not null,
+id_artista int not null,
+nome varchar(255) not null,
+primary key (id_artista),
+foreign key (id_gravadora) references gravadora (id_gravadora));
+
+create table disco(
+id_disco int not null,
+titulo varchar(255) not null,
+data date not null,
+formato varchar(2) not null,
+id_musico varchar(11) not null,
+id_gravadora int not null,
+foreign key (id_musico) references musico (numero_bi),
+foreign key (id_gravadora) references gravadora(id_gravadora),
+primary key (id_disco));
+
+create table genero(
+id_genero int not null,
+nome varchar(155),
+primary key (id_genero));
+
+create table musica(
+id_musica int not null,
+titulo varchar(255) not null,
+autor varchar(255) not null, 
+id_disco int not null,
+id_genero int not null,
+id_artista int not null,
+primary key (id_musica),
+foreign key (id_genero) references genero (id_genero),
+foreign key (id_artista) references artista (id_artista),
+foreign key (id_disco) references disco (id_disco));
+
+create table participacao(
+id_musica int not null,
+numero_bi varchar(11) not null,
+funcao varchar(155) not null,
+primary key (id_musica, numero_bi));
+
+create table plano(
+id_plano int not null,
+nome varchar(155) not null,
+limite_downloads int not null,
+valor decimal (8,5),
+primary key (id_plano));
+
+create table cliente(
+id_cliente int not null,
+id_plano int not null,
+nome varchar(155) not null,
+cliente_telefone varchar(20),
+primary key(id_cliente),
+foreign key (id_plano) references plano(id_plano));
+
+create table download(
+id_download int not null,
+id_cliente int not null,
+id_musica int not null,
+primary key (id_download),
+foreign key (id_cliente) references cliente(id_cliente),
+foreign key (id_musica) references musica(id_musica));
